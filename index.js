116a6726885354e7ded5c8be3c3caf5b8880ff9e
David Nelson Murray <linearsix@gmail.com>
2018-08-09 10:29:51 -0500

    solved part 1

--- index.js
+++ index.js
@@ -2,4 +2,22 @@
   'use strict';
 
   // YOUR CODE HERE
+const btnStop = document.getElementById('stopButton');
+const btnSlow = document.getElementById('slowButton');
+const btnGo = document.getElementById('goButton');
+
+const lgtStop = document.getElementById('stopLight');
+const lgtSlow = document.getElementById('slowLight');
+const lgtGo = document.getElementById('goLight');
+// let stopToggle = 0;
+    // console.log(lights);
+  btnStop.addEventListener('click', (event) => {
+    lgtStop.classList.toggle(`stop`);
+  });
+  btnSlow.addEventListener('click', (event) => {
+    lgtSlow.classList.toggle(`slow`);
+  });
+  btnGo.addEventListener('click', (event) => {
+    lgtGo.classList.toggle(`go`);
+  });
 })();
